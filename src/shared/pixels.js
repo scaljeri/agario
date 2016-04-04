1d65e59cb4dcd7983040fb55a8ad868232cde2fb
Lucas Calje <lucas@frontmen.nl>
2016-04-04 22:25:36 +0200

    Refactored the pixels interface and internals

--- src/shared/pixels.js
+++ src/shared/pixels.js
@@ -1,52 +1,62 @@
 import isNumber from './utils';
 
 /**
- * A Pixel consists of 4 values: RGBA
+ * Pixels live in 2D (x and y), but are represented in a 1D array, using RGBA values
+ *
  * @example
  *     100
  *     0
  *     233
  *     255
  *
- * For performance it is possible to only work with the `R` value and thus, remove
- * the others. This is controlled with the `stride`. Initially, the pixels provided
- * by the canvas have a stride of 4, but it is recommended to set it to 1.
+ * In 1D: [100, 0, 233, 255, 0, 100, 100, 255, ....]
  *
- * Internally the array is a [ndarray]{@link http://docs.scipy.org/doc/numpy/reference/arrays.ndarray.html}
+ * To reach the second pixel 4 pixels need to be skipped, which is known as the `STRIDE`.
+ * The default stride is 4.
  */
 export default class Pixels {
     /**
      * @constructor
-     * By default it is possible to go from a stride of 4 to 2 or 1, but you cannot go from 1 to 4,
-     * because the `GBA` pixels are gone. However, with `cachePixels` turned on, the original
-     * pixels are kept in memory and the stride can go back and forth
-     *
-     * @param {Boolean=false} cachePixels - Keep original pixels
      */
-    constructor(cachePixels) {
-        /**
-         * @private
-         */
-        this.cache = cachePixels;
-    }
+    constructor() {}
 
+    /**
+     * Access the RGBA array by `x` and `y`
+     *
+     * @param {Numer} x
+     * @param {Number} y
+     * @returns {Number}
+     */
     get(x, y) {
-
+        let step = (this.width * y + x) << 2;
+        return this._pixels[step];
     }
 
-    all() {
-        let data = this.data;
+    /**
+     * Returns an ndarray object (https://github.com/scijs/ndarray). For a stride of 4
+     * the original pixels (RGBA) are returned. For a stride of 1 only the `R` values are
+     * returned.
+     *
+     * @param {Number} stride - Supported values are 1 and 4
+     * @returns {{data: *, shape: number[], stride: number[], offset: number}}
+     */
+    ndarray(stride = 4) {
+        if (stride !== 1 && stride !== 4) {
+            throw Error(`Stride should be 1 or 4 (received: ${stride}`);
+        }
 
-        if (this._stride !== this._data.stride[0]) {
-
-            data = this.data.reduce((newData, value, index) => {
+        return {
+            data: stride === 4 ? this._pixels : this._pixels.reduce((newData, value, index) => {
                 if (index % 4 === 0) {
                     newArr.push(value);
                 }
 
                 return newArr;
-            }, []);
-        }
+            }),
+            shape: [this.width, this.height, 4],
+            stride: [4, 4 * this.width, 1],
+            offset: 0
+        };
     }
 
     get height() {
@@ -57,60 +67,24 @@ export default class Pixels {
         return this._width;
     }
 
-    get offset() {
-        return this._offset;
-    }
-
     get stride() {
         return this._stride;
     }
 
-    set stride(stride) {
-        if (isNumber(stride)) {
-            if (stride > 4 || stride <= 0) {
-                throw Error(`Stride should be between 1 and 4! (received: ${stride}`);
-            } else if (this._data) {
-                if (this._stride > this._data.stride[0] && !this._cached) {
-                    throw Error(`Cannot increase stride to ${stride} (Current stride is ${this._stride})`)
-                }
-            }
-        } else {
-            throw Error(`Stride should be a number! (received: ${stride})`);
-        }
-
-        this._stride = stride;
-    }
-
     /**
-     * The pixel array should be a 1 dimensional array with RGBA values (stride of 4)
+     * The pixel array should be a 1 dimensional array with RGBA values (=stride of 4)
      *
-     * @param pixels - Canvas pixel array
+     * @param pixels - Canvas pixel array (stride of 4)
      * @param {Number} height - height of the canvas
      * @param {Number} width - width of the canvas
      * @returns {{data: (*|data), shape: *[], stride: number[], offset: number}|*}
      */
-    setPixels(pixels, height, width) {
-        if (!Array.isArray(pixels)) {
-            throw Error('Pixels should be an array');
-        }
-
-        if (pixels.length > 0 && pixels.length % 4 === 0) {
-            throw Error(`Pixel array has incorrect length (${pixels.length}`);
-        }
-
-        if (!isNumber(width) || !isNumber(height)) {
-            throw(`Height and width should be numbers (received: height=${height}, width=${width}`);
-        }
-
-        this._width = width;
+    set(pixels, height, width, stride) {
+        this._width  = width;
         this._height = height;
-        this._data = {
-            data: pixels,
-            shape: [width, height, 4],
-            stride: [4,  4 * this.width, 1],
-            offset: 0
-        };
+        this._pixels = pixels;
+        this._stride = stride || pixels.length / (width * height); // Expect 1, 2 or 4
+        this._blShift = Math.floor(this._stride/2);                //  Bitwise left shift value;
     }
-
 }
 
--- test/pixels-spec.js
+++ test/pixels-spec.js
@@ -3,6 +3,7 @@ import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 
+import data from './fixtures/pixels-dummy';
 import Pixels from '../src/shared/pixels';
 
 chai.use(chaiAsPromised);
@@ -12,15 +13,39 @@ let should = chai.should();
 
 
 describe('Pixels:', () => {
-    let pixels = new Pixels();
+    let pixels;
 
     beforeEach(() => {
+        pixels = new Pixels();
+        //pixels.set(data);
     });
 
     afterEach(() => {
     });
 
-    it('should not be possible to create a new instance', () => {
-        'true'.should.equal('true');
+    describe('#set', () => {
+        it('should not be possible to set a pixel object', () => {
+            (() => {
+                pixels.set({0: 1, 1: 2, 3: 4}, 1, 1);
+            }).should.throw(Error, /First argument should be an array of pixels/);
+        });
+
+        it('should not be possible to set null as the first argument', () => {
+            (() => {
+                pixels.set(null, 1, 1);
+            }).should.throw(Error, /First argument should be an array of pixels/);
+        });
+
+        it('should not be possible set a String as the first argument', () => {
+            (() => {
+                pixels.set('pixels', 1, 1);
+            }).should.throw(Error, /First argument should be an array of pixels/);
+        });
+
+        it('should not be possible set a Number as the first argument', () => {
+            (() => {
+                pixels.set(1, 1);
+            }).should.throw(Error, /First argument should be an array of pixels/);
+        });
     });
 });
